91023af3e1cc079b7d50d127af872120de44ebdc
Montre0526 <msh9870@gmail.com>
2024-06-25 17:23:09 +0900

    admin.js마무리 trainer.js 마무리

--- bodytrue_back/routes/admin.js
+++ bodytrue_back/routes/admin.js
@@ -9,39 +9,311 @@ const sql = require('../sql.js');
 
 router.post("/패스명",async(req,res)=>{
 
-    db.query("select ")
-})
+
+    db.query("select user_email,user_pwd,user_name,user_tel,user_sex,user_add1,user_add2 from user",(err,results)=>{
+        if (err) {
+            res.send({
+              // 에러 발생 시
+              code: 400,
+              failed: "error occurred",
+              error: err,
+            });
+          } else {
+            res.send(results);
+          }
+    });
+});
 
 //이름 검색하기
 
+router.post("/패스명",async(req,res)=>{
+
+    const name = req.body.name;
+
+    db.query("select user_email,user_pwd,user_name,user_tel,user_sex,user_add1,user_add2 from user where user_name = ?",
+        name,
+        (err,results)=>{
+        if (err) {
+            res.send({
+              // 에러 발생 시
+              code: 400,
+              failed: "error occurred",
+              error: err,
+            });
+          } else {
+            res.send(results);
+          }
+    });
+});
+
 //회원 정지하기
+
+router.post("/패스명",async(req,res)=>{
+
+    const ban_user_no = req.body;
+
+    db.query("update user set user_ban = 1 where user_no = ?",
+        ban_user_no,
+        (err,results)=>{
+        if (err) {
+            res.send({
+              // 에러 발생 시
+              code: 400,
+              failed: "error occurred",
+              error: err,
+            });
+          } else {
+            res.send(results);
+          }
+    });
+    
+});
 
 //트레이너리스트 불러오기
 
+router.post("/패스명",async(req,res)=>{
+
+
+    db.query("select tr_email,tr_pwd,tr_name,tr_tel,tr_sex,tr_add1,tr_add2 from trainer",(err,results)=>{
+        if (err) {
+            res.send({
+              // 에러 발생 시
+              code: 400,
+              failed: "error occurred",
+              error: err,
+            });
+          } else {
+            res.send(results);
+          }
+    });
+});
+
+
 //승인권한 변경
+
+router.post("/패스명",async(req,res)=>{
+
+    const admit_tr_no = req.body;
+
+    db.query("update user set tr_admit = 1 where tr_no = ?",
+        admit_tr_no,
+        (err,results)=>{
+        if (err) {
+            res.send({
+              // 에러 발생 시
+              code: 400,
+              failed: "error occurred",
+              error: err,
+            });
+          } else {
+            res.send(results);
+          }
+    });
+    
+});
 
 //트레이너 삭제
 
+router.post("/패스명",async(req,res)=>{
+
+    const del_tr_no = req.body;
+
+    db.query("delete from trainer where tr_no = ?",
+        del_tr_no,
+        (err,results)=>{
+        if (err) {
+            res.send({
+              // 에러 발생 시
+              code: 400,
+              failed: "error occurred",
+              error: err,
+            });
+          } else {
+            res.send(results);
+          }
+    });
+    
+});
+
 //리뷰리스트 불러오기
+
+router.post("/패스명",async(req,res)=>{
+
+    db.query("select tr_email,tr_pwd,tr_name,tr_tel,tr_sex,tr_add1,tr_add2 from trainer",(err,results)=>{
+        if (err) {
+            res.send({
+              // 에러 발생 시
+              code: 400,
+              failed: "error occurred",
+              error: err,
+            });
+          } else {
+            res.send(results);
+          }
+    });
+});
 
 //리뷰 상세내용 불러오기
 
 //삭제하기
 
-//즐문 등록하기
+//질문 등록하기
 
-//답글 등록하기
+router.post("/패스명",async(req,res)=>{
+
+    const Q = req.body.Q
+
+    db.query("insert into faq faq_q values ?",
+        Q,
+        (err,results)=>{
+        if (err) {
+            res.send({
+              // 에러 발생 시
+              code: 400,
+              failed: "error occurred",
+              error: err,
+            });
+          } else {
+            res.send(results);
+          }
+    });
+    
+});
+
+//답글 등록하기     얘 좀 이상함 동네사람들 이것좀보세요
+
+router.post("/패스명",async(req,res)=>{
+
+    const data={
+        A : req.body.A,
+        faq_no : req.body
+    }
+
+    db.query("update faq set faq_a = ? where faq_no = ?",
+        [data.A , data.faq_no],
+        (err,results)=>{
+        if (err) {
+            res.send({
+            // 에러 발생 시
+            code: 400,
+            failed: "error occurred",
+            error: err,
+            });
+        } else {
+            res.send(results);
+        }
+    });
+})
+
 
 //질문 불러오기
 
+router.post("/패스명",async(req,res)=>{
+    
+    db.query("select faq_q from faq",(err,results)=>{
+        if (err) {
+            res.send({
+              // 에러 발생 시
+              code: 400,
+              failed: "error occurred",
+              error: err,
+            });
+          } else {
+            res.send(results);
+          }
+    });
+});
+
+
 //답글 불러오기
+
+router.post("/패스명",async(req,res)=>{
+    
+    db.query("select faq_a from faq",(err,results)=>{
+        if (err) {
+            res.send({
+              // 에러 발생 시
+              code: 400,
+              failed: "error occurred",
+              error: err,
+            });
+          } else {
+            res.send(results);
+          }
+    });
+});
 
 //질문 수정하기
 
+router.post("/패스명",async(req,res)=>{
+
+    const data={
+        Q : req.body.Q,
+        faq_no : req.body
+    }
+
+    db.query("update faq set faq_q = ? where faq_no = ?",
+        [data.Q , data.faq_no],
+        (err,results)=>{
+        if (err) {
+            res.send({
+            // 에러 발생 시
+            code: 400,
+            failed: "error occurred",
+            error: err,
+            });
+        } else {
+            res.send(results);
+        }
+    });
+})
+
 //답글 수정하기
+
+router.post("/패스명",async(req,res)=>{
+
+    const data={
+        A : req.body.A,
+        faq_no : req.body
+    }
+
+    db.query("update faq set faq_a = ? where faq_no = ?",
+        [data.A , data.faq_no],
+        (err,results)=>{
+        if (err) {
+            res.send({
+            // 에러 발생 시
+            code: 400,
+            failed: "error occurred",
+            error: err,
+            });
+        } else {
+            res.send(results);
+        }
+    });
+})
 
 //FAQ 삭제하기
 
+router.post("/패스명",async(req,res)=>{
+
+    const faq_no = req.body
+
+    db.query("delete from faq where faq_no = ?",
+        faq_no,
+        (err,results)=>{
+        if (err) {
+            res.send({
+            // 에러 발생 시
+            code: 400,
+            failed: "error occurred",
+            error: err,
+            });
+        } else {
+            res.send(results);
+        }
+    });
+})
 
 //승호작성완
 
--- bodytrue_back/routes/trainer.js
+++ bodytrue_back/routes/trainer.js
@@ -5,6 +5,147 @@ const sql = require('../sql.js');
 
 //승호작성
 
+//프로그램 사진업로드/패스저장
+
+
+
+//프로그램 정보 저장
+
+router.post("/패스명(bodytrue_join.html)", async (req, res) => {
+
+    const data = {
+      //프론트에서 전달해주는 데이터
+      pro_name : req.body.prcn_text,
+      pro_tel : req.body.phn_text,
+      pro_add : req.body.adddress_text,
+      pro_startdate : req.body.start_date,
+      pro_enddate : req.body.end_date,
+      pro_comment1 : req.body.img_textarea1,
+      pro_comment2 : req.body.img_textarea2,
+      pro_tag : req.body.tag 
+    };
+  
+    db.query("INSERT INTO PROGRAM (PRO_NAME, PRO_TEL,PRO_ADD1,PRO_STARTDATE,PRO_ENDDATE,PRO_COMMENT1,PRO_COMMENT2,PRO_TAG)values ?,?,?,?,?,?,?,?",
+        [data.pro_name, data.pro_tel, data.pro_add, data.pro_startdate, data.pro_enddate, data.pro_comment1, data.pro_comment2, data.pro_tag], 
+        (err, results, fields) => {
+            //쿼리 실행
+            if (err) {
+                res.send({
+                // 에러 발생 시
+                code: 400,
+                failed: "error occurred",
+                error: err,
+                });
+            } else {
+                res.send({
+                //쿼리 실행 성공시
+                code: 200,
+                message: "프로그램등록 성공",
+                });
+            }
+    });
+});
+
+//내프로그램 리스트
+
+router.post("/패스명", async(req,res)=>{
+
+    const tr_no = req.params.tr_no
+
+    db.query("SELECT PRO_NAME,PRO_TAG,PRO_STARTDATE,PRO_ENDDATE FROM PROGRAM WHERE PRO_TR_NO = ?;",tr_no,(err,results)=>{
+        if (err) {
+            res.send({
+            // 에러 발생 시
+            code: 400,
+            failed: "error occurred",
+            error: err,
+            });
+        } else {
+            res.send
+        }
+    })
+})
+
+//내 리뷰 리스트
+
+router.post("/패스명", async(req,res)=>{
+
+    const tr_no = req.params.tr_no
+
+    db.query("SELECT USER_NAME,PRO_NAME,RE_DATE,RE_COMMENT,RE_RATE FROM REVIEW R JOIN USER U ON R.RE_USER_NO = U.USER_NO JOIN PROGRAM P ON R.RE_PRO_NO = P.PRO_NO JOIN TRAINER T ON R.RE_TR_NO = T.TR_NO WHERE TR_NO = ?;",tr_no,(err,results)=>{
+        if (err) {
+            res.send({
+            // 에러 발생 시
+            code: 400,
+            failed: "error occurred",
+            error: err,
+            });
+        } else {
+            res.send
+        }
+    })
+})
+
+    db.query("이미지 가져오는거 만들어야 해양")
+
+//리뷰 사진 업로드/패스저장
+
+//트레이너 정보 가져오기
+
+//정보
+router.post("/패스명", async(req,res)=>{
+
+    const tr_no = req.params.tr_no
+
+    db.query("SELECT TR_NAME,TR_EMAIL,TR_TEL FROM TRAINER WHERE TR_NO = ?;",tr_no,(err,results)=>{
+        if (err) {
+            res.send({
+            // 에러 발생 시
+            code: 400,
+            failed: "error occurred",
+            error: err,
+            });
+        } else {
+            res.send
+        }
+    })
+})
+
+//사진은 가져오는걸로
+
+
+
+//트레이너 정보수정
+
+router.post("/패스명", async(req,res)=>{
+
+    const tel = req.body.number1 + '-' + req.body.number2 + '-' + req.body.number3;
+    const data={
+        tr_pwd : req.body.password1,
+        tr_addno : req.body.postcode,
+        tr_add1 : req.body.address,
+        tr_add2 : reqq.body.address-detail,
+        tr_tel : tel,
+        tr_no : req.params.tr_no
+    }
+
+    db.query("UPDATE TRAINER SET TR_PWD = ?, TR_ADDNO=?,TR_ADD1=?,TR_ADD2=?,TR_TEL = ? WHERE TR_NO=?;",
+        [data.tr_pwd,data.tr_addno,data.tr_add1,data.tr_add2,data.tr_tel,data.tr_no],
+        (err,results)=>{
+        if (err) {
+            res.send({
+            // 에러 발생 시
+            code: 400,
+            failed: "error occurred",
+            error: err,
+            });
+        } else {
+            res.send
+        }
+    })
+})
+//사진추가해야해양
+
 
 //승호작성완
 
