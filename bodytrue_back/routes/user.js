685b5710e04aa1347c0fff887732e6258bbefe7b
eunmi <mcspygirl2@naver.com>
2024-06-26 17:41:04 +0900

    eu/24-06-26

--- bodytrue_back/routes/user.js
+++ bodytrue_back/routes/user.js
@@ -2,6 +2,8 @@ const express = require('express');
 const router = express.Router();
 const db = require('../db.js');
 const sql = require('../sql.js');
+const { resolve } = require('path');
+const { rejects } = require('assert');
 
 //승호작성
 
@@ -22,18 +24,74 @@ router.get('/prodetail/:pro_no', function(request, response, next){
     // console.log(`Received request for PRO_NO: ${request.params.pro_no}`);
     // console.log(pro_no);
 
-    //pro_no을 통해서 pro_detail 가져옴 
-    db.query(sql.pro_detail,
-        [pro_no], function(error, results, fields){
-            if(error){
-                console.error(error);
-                return response.status(500).json({ error : '상품 정보 에러' });
-            }
-            response.json(results);
+
+    //쿼리문 여러개 실행해야해서 async/await 사용
+    async function getProductDetail(pro_no){
+        try{
+            //상품 상세정보 가져오기
+            const productDetails = await new Promise((resolve, reject) => {
+                db.query(sql.pro_detail, [pro_no], (error, results) => {
+                    if(error) {
+                        return reject(error);
+                    }
+                    resolve(results);
+                });
+            });
+
+            //리뷰 정보 가져오기
+            const reviews = await new Promise((resolve, reject) => {
+                db.query(`select user_name, pro_name, re_rate, re_comment, date_format(re_date,'%y-%m-%d') as re_date
+                            from review r
+                            join user u on r.re_user_no = u.user_no
+                            join program p on r.re_pro_no = p.pro_no
+                            where pro_no = ?`,[pro_no],(error, results) => {
+                    if(error){
+                        return reject(error);
+                    }
+                    resolve(results);
+                });
+            });
+
+            //두 쿼리문 결과 합쳐서 응답보내기
+            response.json({
+                productDetails,
+                reviews
+            });
+
+        } catch(error){
+            console.error(error);
+            response.status(500).json({ error: '서버에러'});
         }
-    )
+    }
+    getProductDetail(pro_no);
+
+
+    //pro_no을 통해서 pro_detail 가져옴 
+    // db.query(sql.pro_detail,
+    //     [pro_no], function(error, results, fields){
+    //         if(error){
+    //             console.error(error);
+    //             return response.status(500).json({ error : '상품 정보 에러' });
+    //         }
+    //         response.json(results);
+    //     }
+    // );
+    // db.query(`SELECT USER_NAME,PRO_NAME,RE_RATE,RE_COMMENT,RE_DATE
+    // FROM REVIEW R JOIN USER U ON R.RE_USER_NO = U.USER_NO JOIN PROGRAM P ON R.RE_PRO_NO = P.PRO_NO
+    // WHERE PRO_NO = ?;`, [pro_no], function(error, results, fields){
+    // if(error){
+    //     console.error(error);
+    //     return response.status(500).json({ error: '리뷰 정보 에러'});
+    // }
+    // response.json(results);
+    // });
     
 });
+
+// router.post("/calendarin", function(request, response, next)=>{
+
+// });
+
 
 //은미작성완
 
